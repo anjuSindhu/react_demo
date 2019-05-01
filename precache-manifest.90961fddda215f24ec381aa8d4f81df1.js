076e8cb1b942d20018b6db30a4068f1df5000245
anjuSindhu <writetoanju@gmail.com>
2019-05-01 14:33:39 +0200

    Updates

---
+++ precache-manifest.90961fddda215f24ec381aa8d4f81df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc78c5dcbc56e4e63c6f450ad5364c4",
+    "url": "/react_demo/index.html"
+  },
+  {
+    "revision": "2afb6ff20b2ebf4403f9",
+    "url": "/react_demo/static/css/main.aacf2a8a.chunk.css"
+  },
+  {
+    "revision": "6a19788b24a7f7504e96",
+    "url": "/react_demo/static/js/2.fad2b514.chunk.js"
+  },
+  {
+    "revision": "2afb6ff20b2ebf4403f9",
+    "url": "/react_demo/static/js/main.ddc84c67.chunk.js"
+  },
+  {
+    "revision": "8a2316504c30c587cbee",
+    "url": "/react_demo/static/js/runtime~main.27d5aae1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
